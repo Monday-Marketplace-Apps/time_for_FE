2a380e65c97168f6d6812976ff873a39fbb1a938
MiguelDN <mpsdiaz@gmail.com>
2022-10-10 19:10:57 +0200

    seleccion datos

--- timetracking-forecast-front/src/pages/app/TimetrackingForecast.jsx
+++ timetracking-forecast-front/src/pages/app/TimetrackingForecast.jsx
@@ -4,6 +4,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 
 import Footer from '../../components/base/Footer';
+import BasicBreadcrumbs from '../../components/app/BasicBreadcrumbs';
+import PureComponent from '../../components/app/Example'
 
 export default function TimetrackingForecast() {
 
@@ -11,7 +13,8 @@ export default function TimetrackingForecast() {
     <React.Fragment>
       <CssBaseline/>
       <Typography variant="h3" component="h1" gutterBottom>
-      Timetracking Forecast App
+      <BasicBreadcrumbs />
+        <PureComponent />
       </Typography>
       <Footer/>
     </React.Fragment>
